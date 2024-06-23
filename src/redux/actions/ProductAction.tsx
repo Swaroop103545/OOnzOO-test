dddb59992ae4a1383af95b5b33a7fbe5ed5ec76f
Swaroop103545 <swaroopprataparao@gmail.com>
2024-06-23 18:32:44 +0530

    implemented axios calling and refactored unwanted code

--- src/redux/actions/ProductAction.tsx
+++ src/redux/actions/ProductAction.tsx
@@ -1,3 +1,20 @@
-export const ProductDetails = () => ({
-  type: 'PRODUCT_DETAILS'
-})
+import axios from 'axios';
+
+const SET_PRODUCTS = 'SET_PRODUCTS';
+
+export const setProducts = (products) => ({
+  type: SET_PRODUCTS,
+  payload: products,
+});
+
+export const fetchProducts = () => {
+  return async (dispatch) => {
+    try {
+      const response = await axios.get('https://fakestoreapi.com/products');
+      dispatch(setProducts(response.data));
+    } catch (error) {
+      console.error('Error fetching products:', error);
+    }
+  };
+};
+
